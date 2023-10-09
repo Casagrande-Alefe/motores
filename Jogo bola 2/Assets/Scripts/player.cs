7de6a389bbe3c52c775210952e93a8318a03a7f7
Álefe de Oliveira Pereira <20221170150027@ifrn.local>
2023-10-09 14:30:47 -0300

    Jogo 1

--- Jogo bola 2/Assets/Scripts/player.cs
+++ Jogo bola 2/Assets/Scripts/player.cs
@@ -7,12 +7,21 @@ public class player : MonoBehaviour
 {
 
  public int velocidade = 10;
+ public int forcaPulo = 7;
  private Rigidbody rb;
+
+ public bool noChao;
 
     // Start is called before the first frame update
     void Start()
     {
      TryGetComponent(out rb);   
+    }
+
+    void OnCollisionEnter(Collision col){
+      if(col.gameObject.tag == "chao"){
+         noChao = true;
+      }
     }
 
     // Update is called once per frame
@@ -22,6 +31,11 @@ public class player : MonoBehaviour
      float inputVertical = Input.GetAxis("Vertical");
 
      rb.AddForce(new Vector3(inputHorizontal,0,inputVertical)* velocidade );  
+
+     if(Input.GetKeyDown(KeyCode.Space) && (noChao == true)){
+         rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+         noChao = false;
+     }
 
      if (transform.position.y <= - 10){
 
