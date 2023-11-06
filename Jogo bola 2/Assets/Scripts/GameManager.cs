dc1a51f7ab29ea64d2cd0b2899789ebd6ef3fecd
Álefe de Oliveira Pereira <20221170150027@ifrn.local>
2023-11-06 14:38:25 -0300

    jogo bola 2

--- Jogo bola 2/Assets/Scripts/GameManager.cs
+++ Jogo bola 2/Assets/Scripts/GameManager.cs
@@ -1,14 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+
+    public TextMeshProUGUI hud, msgVitoria;
+    public int restantes;   
+    public AudioClip clipMoeda, clipVitoria;
+    private AudioSource source;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        restantes = FindObjectsOfType<Moeda>().Length;
+        hud.text = $"Moedas restantes: {restantes}";
+        TryGetComponent(out source);
     }
+
+    public void SubtrairMoedas(int valor)
+
+{
+    
+	restantes = restantes - valor;
+    source.PlayOneShot(clipMoeda);
+	hud.text = $"Moedas restantes: {restantes}";
+    
+
+	if(restantes <= 0)
+{
+    hud.text = " ";
+    
+    source.Stop();
+
+    source.PlayOneShot(clipVitoria);
+
+	msgVitoria.text = "Parabéns!!!";
+}
+
+}
+
 
     // Update is called once per frame
     void Update()
--- Jogo bola 2/Assets/Scripts/Moeda.cs
+++ Jogo bola 2/Assets/Scripts/Moeda.cs
@@ -14,6 +14,9 @@ public class Moeda : MonoBehaviour
 
     private void OnTriggerEnter(Collider other){
         if (other.tag == "Player"){
+
+            FindObjectOfType<GameManager>().SubtrairMoedas(1);
+
             Destroy(gameObject);
         }
     }
